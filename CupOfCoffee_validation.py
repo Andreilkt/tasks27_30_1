e360ea58d249d5673d70b551b09bf7256ad430d7
user <andreilkt@yandex.ru>
2023-07-03 16:46:57 +0700

    Добавлены файлы

---
+++ CupOfCoffee_validation.py
@@ -0,0 +1,22 @@
+from exceptions import TooSmallValue, TooBigValue
+
+class CupOfCoffee:
+    def __init__(self, size_ml, filled_ml):
+        self.size_ml = size_ml
+        self.filled_ml = filled_ml
+
+        # Validation for the filled_ml value
+        if self.filled_ml > self.size_ml:
+            raise TooBigValue(self.size_ml, 'filled_ml is too big!')
+        elif self.filled_ml < 0:
+            raise TooSmallValue(0, 'filled_ml cannot be negative!')
+
+
+
+def fill_cup_of_coffee(cup, ml):
+    try:
+        cup.filled_ml = ml
+    except TooSmallValue as error:
+        print('Filling error:', error)
+    except TooBigValue as error:
+        print('Filling error:', error)
---
+++ validationError.py
@@ -0,0 +1,18 @@
+#https://#stackoverflow.com/questions/22765313/when-import-docx-in-python3-3-i-
+# have-error-importerror-no-module-named-excepti
+
+from exceptions import ValueError
+
+class ValidationError(ValueError):
+    def __init__(self, message):
+        ValueError.__init__(self, message)
+
+class TooSmallValue(ValidationError):
+    def __init__(self, min_val, msg):
+        ValidationError.__init__(self, msg)
+        self.min_val = min_val
+
+class TooBigValue(ValidationError):
+    def __init__(self, max_val, msg):
+        ValidationError.__init__(self, msg)
+        self.max_val = max_val
